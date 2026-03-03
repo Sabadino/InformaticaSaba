1f63bad74cf711cd7c61f091d6512b9fa47941b9
Sabadino <hadikhammoud@icloud.com>
2026-03-03 08:29:24 +0100

    Database

--- ProgettoFinale_Ilmondodellauto/DB_Ilmondodellauto.sql
+++ ProgettoFinale_Ilmondodellauto/DB_Ilmondodellauto.sql
@@ -3,8 +3,8 @@ CREATE DATABASE Ilmondodellauto;
 
 USE Ilmondodellauto;
 
-CREATE TABLE Auto (
-    ID_Auto VARCHAR(255) PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Macchine (
+    ID_Macchine VARCHAR(255) PRIMARY KEY AUTO_INCREMENT,
     Marca VARCHAR(255) NOT NULL,
     Modello VARCHAR(255) NOT NULL,
     Anno_Produzione INT NOT NULL,
@@ -20,17 +20,35 @@ CREATE TABLE Auto (
     Prezzo DECIMAL(10, 2) NOT NULL,
 );
 
-CREATE TABLE User (
+CREATE TABLE Utenti (
    Id_User VARCHAR(255) PRIMARY KEY AUTO_INCREMENT,
     Nome VARCHAR(255) NOT NULL,
     Cognome VARCHAR(255) NOT NULL,
     Email VARCHAR(255) NOT NULL UNIQUE,
-    Username VARCHAR(255) NOT NULL,
+    Username VARCHAR(255) NOT NULL UNIQUE,
     Phone_Number VARCHAR(20) NOT NULL,
     Passwords VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Recensioni(
+    Id_Recensioni VARCHAR(255) PRIMARY KEY AUTO_INCREMENT,
+    Data_Recensione DATE NOT NULL,
+    Valutazione INT NOT NULL,
+    Username VARCHAR(255) NOT NULL,
+    Foreign Key (Username) REFERENCES Utenti(Username)
+);
 
+CREATE TABLE Prenotazioni(
+    Id_Prenotazione VARCHAR(255) PRIMARY KEY AUTO_INCREMENT,
+    Tipo_Prenotazione VARCHAR(255) NOT NULL,
+    Id_User VARCHAR(255) NOT NULL,
+    Id_Macchine VARCHAR(255) NOT NULL,
+    Foreign Key (Id_User) REFERENCES Utenti(Id_User),
+    Foreign Key (Id_Macchine) REFERENCES Macchine(ID_Macchine)
+);
 
+CREATE TABLE SalvaDati(
+    Id_User VARCHAR(255) PRIMARY KEY AUTO_INCREMENT,
+    AutoSalvate VARCHAR(255) NOT NULL,
+    Foreign Key (Id_User) REFERENCES Utenti(Id_User)
 );
