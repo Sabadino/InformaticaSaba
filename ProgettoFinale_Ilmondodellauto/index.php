af7d76b30d3a9cc8667d99600fcdcd2875e646cd
Sabadino <hadikhammoud@icloud.com>
2026-05-11 20:49:58 +0200

    Commit

--- ProgettoFinale_Ilmondodellauto/index.php
+++ ProgettoFinale_Ilmondodellauto/index.php
@@ -1,8 +1,19 @@
 <?php
-// pagina principale - reindirizza alla home
 session_start();
-header('Location: /InformaticaSaba/ProgettoFinale_Ilmondodellauto/userpages/catalogo.php');
-exit;
 ?>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Il Mondo dell'Auto - Mestre</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/InformaticaSaba/ProgettoFinale_Ilmondodellauto/style/navbar.css">
+    <link rel="stylesheet" href="/InformaticaSaba/ProgettoFinale_Ilmondodellauto/style/index.css">
+</head>
+<body>
 
-// zemel //
+<?php include 'include/navbar.php'; ?>
+
+</body>
+</html>
